1b5c6f3bffb80a6a17d1ebc290e8317f5cbca704
TravisSlyter <42150524+TravisSlyter@users.noreply.github.com>
2019-04-29 16:52:35 -0600

    task list, delete, clear btn

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
     //add task event
     form.addEventListener('submit', addTask);
+    // remove task event
+    taskList.addEventListener('click', removeTask);
+    // clear tasks event
+    clearBtn.addEventListener('click', clearTasks);
+    // filter tasks event
+    filter.addEventListener('keyup', filterTasks)
 }
 
 //Add Task
@@ -40,4 +46,40 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// remove task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+    //console.log(e.target);
+    if(confirm('Are You Sure?')) {
+        e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// clear tasks
+function clearTasks() {
+    // taskList.innerhtml = '';
+
+    // or faster
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+    //https://jsperf.com/innerhtml-vs-removechild
+}
+
+// filter tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+    //console.log(text);
+    document.querySelectorAll('.collection-item').forEach
+    (function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) != -1){
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
